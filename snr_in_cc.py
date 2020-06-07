b60dd67b8965c0534cd160c9d444bce609bf07f3
David Hunt <davhunt@indiana.edu>
2020-06-07 13:30:18 -0400

    delete comments

--- snr_in_cc.py
+++ snr_in_cc.py
@@ -60,9 +60,9 @@ img = nib.load(sys.argv[1])
 bvals, bvecs = read_bvals_bvecs(sys.argv[2], sys.argv[3])
 
 # make sure any b0 image bvecs are unit rather than zero
-for i in range(0, len(bvals)):
-  if bvecs[i,0] == 0.0 and bvecs[i,1] == 0.0 and bvecs[i,2] == 0.0:
-    bvecs[i,0] = bvecs[i,1] = bvecs[i,2] = 0.577350
+#for i in range(0, len(bvals)):
+#  if bvecs[i,0] == 0.0 and bvecs[i,1] == 0.0 and bvecs[i,2] == 0.0:
+#    bvecs[i,0] = bvecs[i,1] = bvecs[i,2] = 0.577350
 
 gtab = gradient_table(bvals, bvecs)
 
